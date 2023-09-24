18ee33445fb618e66ad0091cc77d1de773e1ac4f
Михаил <misha13022008@gmail.com>
2023-09-24 13:05:34 +0300

    camera position change

--- Assets/Scripts/CameraPositioner.cs
+++ Assets/Scripts/CameraPositioner.cs
@@ -3,8 +3,8 @@ using UnityEngine;
 
 public sealed class CameraPositioner : MonoBehaviour, IInitializable
 {
-    private const float Multiplier = 6f / 10f;
-    private readonly Vector3 _offset = new(-10 * Multiplier, 10 * Multiplier, 10 * Multiplier);
+    private const float Multiplier = 1;
+    private readonly Vector3 _offset = new(-6 * Multiplier, 4 * Multiplier, 6 * Multiplier);
     private readonly Vector3 _rotation = new(30, 135, 0);
 
     private GameManager _gameManager;
